6ab4a9f2f0ab593a425a0c647dbb27a2865bd8e4
Dominik Fuchß <develop@fuchss.org>
2023-07-21 00:54:13 +0200

    Update GPG page

---
+++ src/pages/contact/pgp-key.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import CodeBlock from '@theme/CodeBlock';
+import GPGKey from '!!raw-loader!/pgp-key/DD7EE78CAC98885B734000ADF07E8A60D810A543.asc';
+
+export default function Home() {
+    return (
+        <Layout
+            title={`PGP-Key`}
+            description="PGP-Key of Dominik Fuchß">
+            <header>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    textAlign: 'left',
+                    marginTop: '15px',
+                }}>
+                    <h1>PGP-Key (0xD810A543)</h1>
+                </div>
+            </header>
+            <main>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    textAlign: 'left',
+                    marginTop: '15px',
+                    marginBottom: '15px',
+                }}>
+                    <CodeBlock language="text">{GPGKey}</CodeBlock>
+                </div>
+            </main>
+        </Layout>
+    );
+}
