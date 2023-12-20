5b1f731b605c24678dc8b52314f96f0c7174c8e5
dfuchss <develop@fuchss.org>
2023-12-20 20:31:35 +0000

    Deploying to gh-pages from @ dfuchss/fuchss.org@c49cc65fe544e14cf3fd5edf3e82fedccd1336c3 🚀

--- assets/js/9c06d5ad.6a19700a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfuchss_org=self.webpackChunkfuchss_org||[]).push([[9975],{9783:e=>{e.exports=JSON.parse('{"title":"Literature","slug":"/category/literature","permalink":"/category/literature","navigation":{"previous":{"title":"PerOpteryx","permalink":"/projects/archived-projects/peropteryx"},"next":{"title":"2023","permalink":"/category/2023"}}}')}}]);
