ea0621a29b022df4eefb45dcc0a66d57a9cb8b11
Dominik Fuchß <develop@fuchss.org>
2026-05-26 00:53:11 +0200

    Update modules for modification

--- update-subtrees.sh
+++ update-subtrees.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git subtree pull --prefix=_modules/jekyll-socials https://github.com/george-gca/jekyll-socials --squash
-
+git subtree pull --prefix=_modules/jekyll-socials https://github.com/george-gca/jekyll-socials main --squash
+git subtree pull --prefix=_modules/al-folio-core https://github.com/al-org-dev/al-folio-core main --squash
