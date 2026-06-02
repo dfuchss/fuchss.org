3b074b70f6a19d20728ce72e0e0e094b71391266
dfuchss <develop@fuchss.org>
2026-06-02 09:48:26 +0000

    Deploying to gh-pages from @ dfuchss/fuchss.org@72ff2eebe2040ab1fdd5d1e3418af85e5e0fe4eb 🚀

--- assets/js/photoswipe-setup.js
+++
@@ -1,8 +0,0 @@
-import PhotoSwipeLightbox from "/assets/libs/photoswipe-lightbox/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "/assets/libs/photoswipe/photoswipe.esm.min.js";
-const photoswipe = new PhotoSwipeLightbox({
-  gallery: ".pswp-gallery",
-  children: "a",
-  pswpModule: PhotoSwipe,
-});
-photoswipe.init();
